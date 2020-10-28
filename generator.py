5a5e395f5c0b3b0206ed8970423d3273930bd17f
Justyna <justyna.pxw@gmail.com>
2020-10-28 19:30:48 +0100

    add input validation

--- generator.py
+++ generator.py
@@ -1,12 +1,21 @@
 from secrets import choice
 import string as st
 
-lenght = int(input("Number of characters: "))
 
-def string_generator(lenght):
+def string_generator():
+    # User's input validation
+    while True:
+        lenght = input("Number of characters: ")
+        try:
+            lenght = int(lenght)
+            break
+        except ValueError:
+            print("Please enter a number")
+
+    # Generate random string
     chars = st.ascii_letters + st.digits
     string = ''.join(choice(chars) for i in range(lenght))
     return string
 
-string = string_generator(lenght)
+string = string_generator()
 print(string)
